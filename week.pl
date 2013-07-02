0c22933a7faaa8402241b5959b3b7cdf72480c16
Jacob Certain <jacob.certain@gilbertschools.net>
2013-07-01 20:00:38 -0700

    rewrote ages ago

--- week.pl
+++ week.pl
@@ -3,60 +3,118 @@
 use warnings;
 use strict;
 
+use DateTime;
 use Config::Tiny;
 use Expect;
-use POSIX qw/strftime/;
 
-my $file = "auth.conf";
-my $start = "021113";
-my $end = "021713";
+my $file = "autoclk.conf";
+my $tz = "-0700";
 
+my $start = "";
+my $end = "";
 
-# read credentials from file
-my $Config = Config::Tiny->read($file);
-my $pass = $Config->{_}->{password}
-  or die "Can't read password from $file. $!\n";
-my $user = $Config->{_}->{username}
-  or die "Can't read username from $file. $!\n";
+my $user = "";
+my $pass = "";
 
+sub main()
+{
+    ($start, $end) = getRange();
+    ($user, $pass) = getLogin();
+    print(unescape(getRaw()));
+}
 
-# init expect
-my $exp = new Expect;
-$exp->raw_pty(0);
-$exp->spawn("tn5250", "district.gps")
-  or die "Can't spawn: $!\n";
-$exp->log_stdout(0);
+sub unescape()
+{
+    my ($str) = @_;
 
-# log in to timeclk
-$exp->expect(1, 'User  . . . . . . . . . . . . . .   ');
-$exp->send("timeclk\ttimeclk\r");
+    # ESC[10;2H means put the following on line 10 of this screen
+    # we don't care which line of what screen
+    # so convert them all to newlines
+    $str =~ s/\e\[\d+;2H/\n /g;
 
-# ask for employee inquiry
-$exp->send("\[21~"); # send F10
-$exp->send("$user\t$pass");
-$exp->send("$start$end\r");
+    # remove remaining ansi escapes
+    $str =~ s/\e\[?.*?[\@-~]//g;
 
-# we're gonna look for a date next
-# look for february as 2, not 02
-my $today = strftime("%m/%d/%y", localtime);
-$today =~ s/^0//;
+    # the kronos output uses ^O as a field delimiter. since parse.pl
+    # uses substr, kill it
+    $str =~ s///g;
 
-# clear all the previous crap and get the result screen
-$exp->clear_accum();
-$exp->expect(1, $today);  # this is on the date picker screen
-$exp->expect(1, $today);  # this is on the result screen
-$exp->expect(1, "11=Charge");
-my $bar = $exp->before();
-$exp->hard_close();
+    return $str;
+}
 
+sub getRange()
+{
+    #my $now = DateTime->now(time_zone=>$tz);
 
-# ESC[10;2H means put the following on line 10 of this screen
-# we don't care which line of what screen
-# so convert them all to newlines
-$bar =~ s/\e\[\d+;2H/\n /g;
-$bar =~ s/\e\[?.*?[\@-~]//g;
-$bar =~ s///g;
-print("$bar\n");
+    my $now = now DateTime (time_zone=>$tz);
+
+    my $monday = $now->clone()->subtract(days => ($now->wday - 1));
+    my $sunday = $now->clone()->add(days => (7 - $now->wday));
+
+    #my $monday = clone
+
+    #subtract $monday (days => 7);
+    #subtract $sunday (days => 7);
+
+    #$monday->subtract(days => 7);
+    #$sunday->subtract(days => 7);
+
+    my $pattern = "MMddyy";
+
+    return ($monday->format_cldr($pattern), $sunday->format_cldr($pattern));
+}
+
+sub getLogin()
+{
+    # read credentials from file
+    my $Config = Config::Tiny->read($file);
+    my $user = $Config->{_}->{username}
+        or die "Can't read username from $file. $!\n";
+    my $pass = $Config->{_}->{password}
+        or die "Can't read password from $file. $!\n";
+
+    return ($user, $pass);
+}
+
+sub getRaw()
+{
+    my $result = "";
+
+    # init expect
+    my $exp = new Expect;
+    $exp->raw_pty(0);
+    $exp->spawn("tn5250", "district.gps")
+        or die "Can't spawn: $!\n";
+    $exp->log_stdout(0);
+
+    # log in to timeclk
+    $exp->expect(1, 'User  . . . . . . . . . . . . . .   ');
+    $exp->send("timeclk\ttimeclk\r");
+
+    # ask for employee inquiry
+    $exp->send("\[21~"); # send F10
+    $exp->send("$user\t$pass");
+    $exp->send("$start$end\r");
+
+    # we're gonna look for a date next (look for feb as 2, not 02)
+    my $today = DateTime->now(time_zone=>$tz)->format_cldr("M/dd/yy");
+
+    # clear all the previous crap
+    $exp->expect(1, $today);  # this is on the inquiry screen
+    $exp->expect(1, $today);  # this is on the result screen
+    $exp->expect(1, "11=Charge");
+
+    # get the result screen
+    $result = $exp->before();
+
+    # kill the connection. soft_close takes several seconds
+    $exp->hard_close();
+
+    # the $today and two spaces were stripped, so add them back
+    return "  ".$today.$result;
+}
+
+main();
 
 
 #original expect source
@@ -76,3 +134,5 @@ print("$bar\n");
 #append output $expect_out(buffer)
 #send_user "$output"
 #close
+
+# vim: ts=4:sts=4:sw=4
